b5a9cc7c43924d0fec5ddecb7fb901371eb3ac86
Ritu Kanchi <ritukanchi04@gmail.com>
2024-02-10 16:39:10 +0530

    updated

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "TEXT-SUMMARIZATION-PROJECT"
-AUTHOR_USER_NAME = "vaibhav071104"
-SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "vaibhavnsingh07@gmail.com"
+AUTHOR_USER_NAME = "ritukanchi"
+SRC_REPO = "Text-Summarization-Project"
+AUTHOR_EMAIL = "ritukanchi04@gmail.com"
 
 
 
